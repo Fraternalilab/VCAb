9ba0bb43d12c2d2691d2de703f9fe79d4997905e
Dongjun Guo <dongjun.name@gmail.com>
2022-04-22 12:09:33 +0100

    run_dssp.sh modified

--- dssp/run_dssp.sh
+++ dssp/run_dssp.sh
@@ -1,3 +1,11 @@
+if [ ! -d dssp_results ];then
+	mkdir dssp_results
+fi
+
+if [ ! -d aln_results ];then
+        mkdir aln_results
+fi
+
 cd dssp_results
 for pdb_file in $(ls ../../pdb_struc/chain_pdb/*.pdb); do mkdssp -i $pdb_file -o $(basename $pdb_file .pdb)".dssp"; done
 cd ..
