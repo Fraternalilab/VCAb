11e3f630fd4886316a37cf81680e6092798b595e
Dongjun Guo <dongjun.name@gmail.com>
2022-10-24 16:22:10 +0100

    update .sh to make blast db

--- vcab_db/mk_vcab_bl_db.sh
+++ vcab_db/mk_vcab_bl_db.sh
@@ -1,31 +1,31 @@
 cd ../seq_db/vcab_db
 
-cp H_seq.fasta h_seq_db
+cp fasta/H_seq.fasta h_seq_db
 cd h_seq_db
 makeblastdb -in H_seq.fasta -dbtype prot
 cd ..
 
-cp L_seq.fasta l_seq_db
+cp fasta/L_seq.fasta l_seq_db
 cd l_seq_db
 makeblastdb -in L_seq.fasta -dbtype prot
 cd ..
 
-cat H_seq.fasta L_seq.fasta > full_seq_db/all_full_seq.fasta
+cat fasta/H_seq.fasta fasta/L_seq.fasta > full_seq_db/all_full_seq.fasta
 cd full_seq_db
 makeblastdb -in all_full_seq.fasta -dbtype prot
 cd ..
 
-cp HV_seq.fasta hv_seq_db
+cp fasta/HV_seq.fasta hv_seq_db
 cd hv_seq_db
 makeblastdb -in HV_seq.fasta -dbtype prot
 cd ..
 
-cp LV_seq.fasta lv_seq_db
+cp fasta/LV_seq.fasta lv_seq_db
 cd lv_seq_db
 makeblastdb -in LV_seq.fasta -dbtype prot
 cd ..
 
-cat HV_seq.fasta LV_seq.fasta > v_seq_db/all_v_seq.fasta
+cat fasta/HV_seq.fasta fasta/LV_seq.fasta > v_seq_db/all_v_seq.fasta
 cd v_seq_db
 makeblastdb -in all_v_seq.fasta -dbtype prot
 cd ..
