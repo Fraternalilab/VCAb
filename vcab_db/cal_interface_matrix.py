c9431bc4e8e679cdced3de53dc1ecf2c3e75715b
Dongjun Guo <dongjun.name@gmail.com>
2024-01-01 10:54:00 +0000

    add the aa_name dict

--- vcab_db/cal_interface_matrix.py
+++ vcab_db/cal_interface_matrix.py
@@ -10,6 +10,12 @@ import math
 from datetime import date
 import json
 
+aa_names= {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+     'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
+     'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
+     'ALA': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M',
+          "-":"-"}
+
 imgt_numbering=['1H',
  '1G',
  '1F',
